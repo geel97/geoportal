69d35c049783377499245c3915c3023801c3cfe7
Erik Geletti <egeletti@inogs.it>
2022-02-21 15:03:33 +0100

    Modifiche a graphs

--- src/app/detail-dialog/graphs/graphs.component.ts
+++ src/app/detail-dialog/graphs/graphs.component.ts
@@ -3,19 +3,11 @@ import * as Highcharts from 'highcharts/highstock';
 import { DataType, ErddapService, Parameter, Measurement } from '../../erddap.service';
 import IndicatorsCore from 'highcharts/indicators/indicators';
 import IndicatorZigzag from 'highcharts/indicators/zigzag';
-import { Options, PlotOptions, PointOptionsObject, SeriesOptionsType } from 'highcharts';
+import { Options } from 'highcharts';
 import Collection from 'ol/Collection';
 import Feature from 'ol/Feature';
 import Geometry from 'ol/geom/Geometry';
 import { VocabService } from '../../vocab.service';
-
-// HIndicatorsAll(Highcharts);
-// HDragPanes(Highcharts);
-// HAnnotationsAdvanced(Highcharts);
-// HPriceIndicator(Highcharts);
-
-IndicatorsCore(Highcharts);
-IndicatorZigzag(Highcharts);
 
 @Component({
   selector: 'app-graphs',
@@ -32,6 +24,47 @@ export class GraphsComponent implements OnInit {
   constructor(private erdappService: ErddapService, public vocabService: VocabService) {
     this.chartOptions = {
       title: { text: undefined },
+      time: {
+        useUTC: false,
+      },
+      rangeSelector: {
+        buttons: [
+          {
+            type: 'hour',
+            count: 1,
+            text: '1h',
+            title: 'View 1 hour',
+          },
+          {
+            type: 'hour',
+            count: 6,
+            text: '6h',
+            title: 'View 6 hour',
+          },
+          {
+            type: 'day',
+            count: 1,
+            text: '1d',
+            title: 'View 1 day',
+          },
+          {
+            type: 'week',
+            text: '1w',
+            title: 'View 1 week',
+          },
+          {
+            type: 'month',
+            count: 1,
+            text: '1m',
+            title: 'View 1 month',
+          },
+          {
+            type: 'all',
+            text: '6m',
+            title: 'View 6 months',
+          },
+        ],
+      },
       xAxis: {
         type: 'datetime',
         title: {
@@ -51,17 +84,17 @@ export class GraphsComponent implements OnInit {
     this.getDataArray(
       this.data.item(0).get('name'),
       { name: 'WSPD', type: DataType.TIME_SERIES },
-      this.daysAgoMidnightUTC(5)
+      this.daysAgoMidnightUTC(120)
     );
   }
 
   getDataArray(dataset: string, parameter: Parameter, timeStart: Date, timeEnd?: Date) {
-    let dataArray = new Array<PointOptionsObject>();
+    let dataArray: number[][] = [];
     this.loading = true;
     this.erdappService.getData(dataset, parameter, false, timeStart, timeEnd).subscribe(
       (response: Measurement[]) => {
         dataArray = response.map((measure: Measurement) => {
-          return { x: new Date(measure.timestamp).getTime(), y: measure.measurement };
+          return [new Date(measure.timestamp).getTime(), measure.measurement];
         });
 
         this.chartOptions.series = [
@@ -69,6 +102,10 @@ export class GraphsComponent implements OnInit {
             name: this.vocabService.getMeasurementName(parameter.name),
             type: 'line',
             data: dataArray,
+            tooltip: {
+              valueDecimals: 2,
+              valueSuffix: this.vocabService.getMeasurementUnit(parameter.name),
+            },
           },
         ];
         this.chartOptions.yAxis = [
